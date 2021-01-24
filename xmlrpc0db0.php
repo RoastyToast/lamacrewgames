62e7da164cf0bb8bff88e05fc892971fece9649a
Philip <gaming4all.play@gmail.com>
2021-01-25 00:26:04 +0100

    Website Commit

---
+++ xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://upex.streamers.site</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://upex.streamers.site/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://upex.streamers.site/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
