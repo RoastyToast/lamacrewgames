0d3c1c9525207ebb5be34b6d51afeefc7f98976c
Philip <gaming4all.play@gmail.com>
2021-01-25 12:56:21 +0100

    Small fix

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -2,13 +2,13 @@
 	<service>
 		<engineName>WordPress</engineName>
 		<engineLink>https://wordpress.org/</engineLink>
-		<homePageLink>https://upex.streamers.site</homePageLink>
+		<homePageLink>https://lamacrew.com</homePageLink>
 		<apis>
-			<api name="WordPress" blogID="1" preferred="true" apiLink="https://upex.streamers.site/xmlrpc.php" />
-			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
-			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
-			<api name="Blogger" blogID="1" preferred="false" apiLink="https://upex.streamers.site/xmlrpc.php" />
-				<api name="WP-API" blogID="1" preferred="false" apiLink="https://upex.streamers.site/wp-json/" />
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://lamacrew.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://lamacrew.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://lamacrew.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://lamacrew.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://lamacrew.com/wp-json/" />
 			</apis>
 	</service>
 </rsd>
